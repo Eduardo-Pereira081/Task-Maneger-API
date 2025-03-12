381e6b2de01771d40322379d688f3a284f8cc504
Eduardo pereira <ant.eduardo081@gmail.com>
2025-03-11 21:17:04 -0300

    add configs and validations data

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
 import express  from "express";
 import { router } from "./routes";
+import { appErrors } from "./errors/appErrors";
+import { error } from "console";
+import { sqliteConnection } from "./databases";
+import { runMigrations } from "./databases/migrations";
 
 const app = express();
 const PORT = process.env.PORT || 3333;
@@ -7,6 +11,17 @@ const PORT = process.env.PORT || 3333;
 app.use(express.json());
 app.use(router);
 
+app.use(appErrors);
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}...`);
 });
+
+sqliteConnection().then(()=>{
+    console.log("Database is connected!");
+})
+.catch((error)=>{
+    console.log("Database ERROR - ", error);
+});
+
+runMigrations();
