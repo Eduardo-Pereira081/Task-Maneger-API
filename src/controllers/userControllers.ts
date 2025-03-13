9a346f9537e85fa9a8afa829e277c5ff17d31ca1
Eduardo pereira <ant.eduardo081@gmail.com>
2025-03-12 21:07:50 -0300

    mod

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,25 +1,29 @@
 import { NextFunction, Request, Response } from "express";
 import { userSchema } from "../validations/userSchema";
+import { userServices } from "../services/userServices";
+import { userRepository } from "../repositories/userRepository";
 
 export const userControllers = {
-    async create(req:Request , res: Response, next:
-        NextFunction ){
-            try{
-                const{ name, email, password }= userSchema.parse(req.body);
-                //Save user to database
-                res.status(201).json({name,email,password});
-            }catch (error){
-                next(error);
-            }
-    },
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { name, email, password } = userSchema.parse(req.body);
 
-    async read(req:Request , res: Response, next:
-        NextFunction ){
-            try{
-                const{ }=req.body;
-                res.status(200).json({message:"User read"});
-            }catch (error){
-                next(error);
-            }
-    },
+     const userCreated = await userServices.create({ name, email, password }, userRepository);
+
+     //SOLID
+
+      res.status(201).json(userCreated);
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  async read(req: Request, res: Response, next: NextFunction) {
+    try {
+      const {} = req.body;
+      res.status(200).json({ message: "User read" });
+    } catch (error) {
+      next(error);
+    }
+  },
 };
